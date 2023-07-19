17c4bac485cb4d1fd3dd09257f2de194f157c364
eikl <kleemolaeino@gmail.com>
2023-07-19 22:39:11 +0300

    removed files

--- super_secret.py
+++
@@ -1,3 +0,0 @@
-#here we store secret stuff
-DATABASE_URL = 'mysql+pymysql://admin:salasana@coffee-data.c1uqaxg5dghw.eu-north-1.rds.amazonaws.com:3306/srf05_data'
-db = pymysql.connect(host='coffee-data.c1uqaxg5dghw.eu-north-1.rds.amazonaws.com', user='admin', password='salasana')
