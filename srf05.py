4525c7b1aafebfc2c60f4a982106e042554ec838
eikl <eino.kleemola@helsinki.fi>
2023-07-20 13:33:08 +0300

    edited readme

--- srf05.py
+++ srf05.py
@@ -41,6 +41,7 @@ def distance():
 	TimeElapsed = StopTime - StartTime
 	# multiply with the sonic speed (34300 cm/s)
 	# and divide by 2, because there and back
+    # ignore the effect of hot air on the speed of sound
 	distance = (TimeElapsed * 34300) / 2
  
 	return distance
