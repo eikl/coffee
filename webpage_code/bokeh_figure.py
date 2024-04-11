1741de1fc19b590a88868c55aeeec2a84afdefed
Eino Kleemola <eino.kleemola@helsinki.fi>
2024-04-11 20:44:21 +0300

    test

--- webpage_code/bokeh_figure.py
+++ webpage_code/bokeh_figure.py
@@ -10,18 +10,20 @@ from bokeh.models import DatetimeTickFormatter, NumeralTickFormatter
 import datetime as dt
 import sql_queries
 
+import numpy as np
+
+
+
 def plot():
     # Prepare some data
 
     df = sql_queries.get_level_data()
-    df2 = sql_queries.get_atm_data()
+    # df2 = sql_queries.get_atm_data()
 
-    x = pd.to_datetime(df["date"])
+    x = pd.to_datetime(df['date'])
     y = df["level"]
     y = y.rolling(window=10).median()
-    y2 = df2["voc"]
-    y22 = df2["rh"]
-    x2 = df2["date"]
+    print(x,y)
     # Create a new plot with a dark background
     p = figure(x_axis_label='Aika', 
                y_axis_label='VOC indeksi',
@@ -31,7 +33,6 @@ def plot():
                background_fill_color = '#2f3640',
                border_fill_color = '#2f3640',
                outline_line_color = '#2f3640',
-               y_range = Range1d(0,500),
                active_drag = None,
                active_scroll = None,
                active_tap = None
@@ -43,14 +44,11 @@ def plot():
                                                  minutes = '%H:%M:%S')
 
     
-    p.line(x2, y2, line_width=2, line_color="#f5f6fa",legend_label="VOC indeksi")
+    p.scatter(x, y,legend_label="VOC indeksi")
 
-    p.extra_y_ranges['rh'] = Range1d(0,100)
-    p.line(x2, y22, color="orange", y_range_name="rh",legend_label="RH")
 
-    ax2 = LinearAxis(y_range_name="rh", axis_label="RH (%)")
-    ax2.axis_label_text_color ="navy"
-    p.add_layout(ax2, 'right')
+
+
 
 
     p.yaxis[0].formatter = NumeralTickFormatter(format='0.00')
@@ -78,4 +76,5 @@ def plot():
     p.legend.background_fill_alpha = 0
     # Generate the components of the plot
     script, div = components(p)
+    #show(p)
     return script,div
