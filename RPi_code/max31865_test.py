f7d82d690b0940fac9dbd55ed4a586c6943450d6
Eino K <kleemolaeino@gmail.com>
2024-07-11 15:03:25 +0300

    temp

--- RPi_code/max31865_test.py
+++ RPi_code/max31865_test.py
@@ -7,11 +7,21 @@ import time
 import board
 import digitalio
 import adafruit_max31865
-
+import pymysql
+import super_secret
+import datetime as dt
+#connect to jeff bezos
+try:
+   db = super_secret.db
+   cursor = db.cursor()
+   cursor.execute('USE srf05_data')
+except:
+   print("Couldn't connect to database")
+   quit()
 
 # Create sensor object, communicating over the board's default SPI bus
 spi = board.SPI()
-cs = digitalio.DigitalInOut(board.D5)  # Chip select of the MAX31865 board.
+cs = digitalio.DigitalInOut(board.D22)  # Chip select of the MAX31865 board.
 sensor = adafruit_max31865.MAX31865(spi, cs)
 # Note you can optionally provide the thermocouple RTD nominal, the reference
 # resistance, and the number of wires for the sensor (2 the default, 3, or 4)
@@ -22,7 +32,11 @@ sensor = adafruit_max31865.MAX31865(spi, cs)
 while True:
     # Read temperature.
     temp = sensor.temperature
-    # Print the value.
-    print("Temperature: {0:0.3f}C".format(temp))
-    # Delay for a second.
-    time.sleep(1.0)
+    print(temp)
+    current_date = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    try:
+        cursor.execute("insert into level_data(date,level) values (%s,%s)",(current_date,temp))
+        db.commit()
+    except:
+        print('could not connect to db')
+    time.sleep(15)
